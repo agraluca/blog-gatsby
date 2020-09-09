669173a5633b7b4f155d19fba235850a9b2a2295
Luca <luca.agra@hotmail.com>
2020-09-09 12:20:17 -0300

    versao incluindo favIcon melhorado

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,7 @@ module.exports = {
         background_color: `#16202c`,
         theme_color: `#16202c`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/faviconT.svg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sitemap`,
